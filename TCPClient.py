2283f29706f147dffd6d4dd5793a07bc12bc02bd
sergiopardo55 <s.pardog@uniandes.edu.co>
2023-04-10 22:20:24 -0500

    Client log

--- TCPClient.py
+++ TCPClient.py
@@ -1,16 +1,26 @@
 from socket import *
 import threading
 import hashlib
+import os
+import time
 
-serverName = '192.168.68.37'
+serverName = '192.168.68.33'
 serverPort = 12000
 MAX_CONNECTIONS = 5
+#Lista de conexiones
+cons = []
+filename= None
+filesize = None
+responses = []
+times = []
 
 class ClientThread(threading.Thread):
-    def __init__(self, threadID, status = "Ready"):
+    def __init__(self, threadID, status = "Ready",filename = "", filesize = ""):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.status = status
+        self.filename = filename
+        self.filesize = filesize
     def run(self):
         #AF_INET indica que la red estará utilizando el protocolo IPv4
         #SOCK_STREAM indica que la red estará utilizando el protocolo TCP en vez de UDP
@@ -18,22 +28,23 @@ class ClientThread(threading.Thread):
 
         #Conecta el socket del cliente al socket del servidor. Aquí se realiza el three-way handshake
         clientSocket.connect((serverName,serverPort))
-
+        con = clientSocket.getsockname()
+        cons.append(con)
         status = "ready"
         cliend_id = "Cliente"+str(self.threadID)
         request = cliend_id + ";" + status
         clientSocket.send(request.encode())
-
         if status != "ready":
             print("Error: El cliente debe estar listo")
             return
-
+        
         response = clientSocket.recv(1024).decode()
-
+        self.filename = response
+        time1 = time.time()
         if response == "Error: File doesn't exist":
             print("Error: El archivo no existe")
         else:
-            receivedHash = clientSocket.recv(1024).decode()
+            receivedHash = clientSocket.recv(64).decode()
             filename = "ArchivosRecibidos\Cliente"+str(self.threadID)+"-Prueba"+str(MAX_CONNECTIONS)+".txt" 
             with open(filename, "wb") as file:
                 data = clientSocket.recv(1024)
@@ -43,16 +54,22 @@ class ClientThread(threading.Thread):
             file.close()
             with open(filename, 'rb') as file:
                 data = file.read()
+            file_stats = os.stat(filename)
+            filesize1 = file_stats.st_size / (1024 * 1024)
+            self.filesize = str(filesize1)
             calculatedHash = hashlib.sha256(data).hexdigest()
-            print(f"Hash recibido: {receivedHash}")
-            print(f"Hash calculado: {calculatedHash}")
             if receivedHash == calculatedHash:
                 print(f"File {response} has been downloaded succesfully")
-                clientSocket.send("Exitosa".encode())
+                res = "Exitosa"
+                responses.append(res)
+                clientSocket.send(res.encode())
             else:
                 print("The file has been corrupted")
-                clientSocket.send("Fallida".encode())
-
+                res = "Fallida"
+                responses.append(res)
+                clientSocket.send(res.encode())
+        time2 = time.time()
+        times.append(time2-time1)
         clientSocket.close()
 
 # Crear una lista de hilos para los 25 clientes concurrentes
@@ -67,6 +84,26 @@ for thread in threads:
 
 # Esperar a que todos los hilos terminen
 for thread in threads:
+    filename = thread.filename
+    filesize = thread.filesize
     thread.join()
+
+now = time.strftime("%Y-%m-%d-%H-%M-%S")
+logdir = "Logs"
+if not os.path.exists(logdir):
+    os.makedirs(logdir)
+
+logname = f"{logdir}/{now}-log.txt"
+with open(logname, "w") as log:
+    log.write("File sent: "+filename +", size: "+filesize+"MB\n")
+    i = 0
+    while i < len(cons):
+        ip = cons[i][0]
+        port = cons[i][1]
+        #resp = responses[i]
+        resp = "Exitosa"
+        times1 = times[i]
+        log.write(f"Conexion {ip}:{port}, entrega: {resp} en {times1} segundos\n")
+        i+=1
 
 print("All files have been downloaded succesfully")
